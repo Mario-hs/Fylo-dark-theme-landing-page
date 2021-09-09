d3bdb23e02e98d5ef1a609882a40d30f0f4c04a8
Mario Henrique <mario-hp41@hotmail.com>
2021-09-09 18:17:06 -0300

    associal icons and scripts

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const email = document.querySelector('#email').addEventListener('keyup', (e) => {
+    console.log(e.target.value);
+});
+
+const confirm = document.querySelector('#started').addEventListener('click', () => {
+    console.log('clicou')
+})
